97bf0312970b0827d5eabc3c9603d3c5c623f493
Yosy Attias <yosy101@gmail.com>
2013-12-15 22:20:52 +0200

    - Refactor morpheus.js - Getting walkes form yosy/walkes for the passing the node as parameter and not scope, when it will get merged and published, we will update to get it from npm

--- src/morphues.js
+++ src/morphues.js
@@ -7,27 +7,74 @@ var Scope = require("./structures/scope");
 var M = require("./utils");
 
 
-exports.analyze = function(sourceFile) {
-  var tree = esprima.parse(sourceFile);
 
-  var result = {};
+function Morpheus() {
+  this.globalScope = new Scope();
+  this.tree = {};
+}
 
-  var globalScope = new Scope();
 
-  walker(tree, {
-    VariableDeclarator: function() {
-        // get the identificatin for this variable declaration
-        // and add it as variable 
-        var identification = M.variableIdentificationsOf(this.init);
-        identification.name = this.id.name;
-        globalScope.addVariable(identification);
-
+_.extend(Morpheus.prototype, {
+  /*
+   * Analyzes the given source file (as string), and returns the
+   * scopes metadata.
+   *
+   * @param  {string} sourceFile - the source file as string
+   * @return {Scope} global scope
+   */
+  analyze : function(sourceFile) {
+    if(!_.isString(sourceFile) || sourceFile.length === 0) {
+      throw new Error("sourceFile: should be non empty string");
     }
 
-  });
+    this.tree = esprima.parse(sourceFile);
+    this._walkTree();
 
+    return this.globalScope;
+  },
 
-  return globalScope;
+  /*
+   * Starts walking the current tree
+   */
+  _walkTree : function() {
+    walker(this.tree, this._getVisitors());
+  },
+
+  /*
+   * Get all visitors, and change their their scope to be "Morpheus" scope
+   * @return {Object} key - visitor name, value - function
+   */
+  _getVisitors : function() {
+    var visitors = {};
+    _.each(this.visitors, function(visitor, name) {
+      visitors[name] = visitor.bind(this);
+    }.bind(this));
+
+    return visitors;
+  },
+
+  /*
+   * The AST visitors, each visitor be walk others ast node types of the tree,
+   * their 'this' scope, will be Morpheus, please see Morpheus.prototype._walkTree
+   */
+  visitors : {
+    /*
+     * Handles variable declarations, simple get infered values from the left side
+     * declarations
+     */
+    VariableDeclarator: function variableDeclarator(node, recurse, stop) {
+      var identification = M.variableIdentificationsOf(node.init);
+      identification.name = node.id.name;
+
+      this.globalScope.addVariable(identification);
+    }
+  }
+
+});
+
+exports.analyze = function(sourceFile) {
+  var morepheus = new Morpheus();
+  return morepheus.analyze(sourceFile);
 };
 
 
